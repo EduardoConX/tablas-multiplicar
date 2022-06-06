34be4d56fcca54df90e03be3f7abeeb0efaed29d
Eduardo Pacheco <38542760+EduardoConX@users.noreply.github.com>
2022-06-05 20:21:30 -0500

    Comments deleted

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ console.clear();
 
 console.log(argv);
 
-/* const [,, arg3 = "base=1"] = process.argv;
-const [, base] = arg3.split("="); */
-
 crearArchivo(argv.b, argv.l, argv.h)
 .then(msg => console.log(msg.rainbow))
 .catch(err => console.log(err));
